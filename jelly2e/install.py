92b46d7e39edcc0e96c34435b682fe7c5289b81d
rumplestilzken <rumplestilzken@gmail.com>
2023-08-31 19:11:52 -0400

    Fixed hanging adb shell request

--- jelly2e/install.py
+++ jelly2e/install.py
@@ -133,7 +133,6 @@ def flash_lineage():
 
     answer = input("Let device power on and manually process according to README. Press Enter to continue.")
     os.system("adb kill-server")
-    os.system("adb shell; exit")
     os.system("adb reboot bootloader")
     answer = input("Press Volume Up on the device when prompted...Press enter to continue")
     os.system("fastboot flashing unlock")
